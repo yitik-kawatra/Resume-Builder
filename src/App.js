2d4bb87db02a17b2f296072840b59ac0c0e8978c
Yitik Kawatra <yitikkawatra@gmail.com>
2021-08-12 19:00:33 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-
 import './static/scss/app.scss';
 import 'react-router-dom';
 import { Route,Switch } from 'react-router-dom';
 import Header from './components/presentation/header';
-import Footer from './components/presentation/footer';
 import LandingPage from './components/presentation/landingPage';
 import GettingStarted from './components/presentation/gettingStarted';
 import Login from './components/presentation/login';
 import Register from './components/presentation/register';
-
 import Contacts from './components/presentation/contact';
 import Education from './components/presentation/education';
 import Finalize from  './components/presentation/finalizePage';
